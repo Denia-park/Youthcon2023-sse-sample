0dd181bfa0d66127b561e5fa402460022bedf499
HyunggiPark <phg9587@naver.com>
2023-10-15 21:46:47 +0900

    fix: Sse 전송안되는 버그 수정
    - Builder를 넘겨야 한다.

--- src/main/java/com/example/youthcon/handson/CommentService.java
+++ src/main/java/com/example/youthcon/handson/CommentService.java
@@ -1,18 +1,13 @@
 package com.example.youthcon.handson;
 
+import com.example.youthcon.preparation.Comment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
-
-import com.example.youthcon.preparation.*;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class CommentService {
@@ -46,7 +41,7 @@ public class CommentService {
 
     private void sendEvent(final SseEmitter sseEmitter, final SseEmitter.SseEventBuilder sseEventBuilder) {
         try {
-            sseEmitter.send(sseEventBuilder.build());
+            sseEmitter.send(sseEventBuilder);
         } catch (IOException e) {
             //서버에서는 클라이언트의 상태를 알 수 없기 때문에, 만약 클라이언트가 Tab을 닫은 상태라면, IO Exception이 발생함
             throw new RuntimeException(e);
